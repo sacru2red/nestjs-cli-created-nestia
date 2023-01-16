60a360cc11055b3cfa35257737924c2da69aca01
sacru2red <sacru2red@gmail.com>
2023-01-16 10:17:03 +0900

    use TypedRoute

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import core from '@nestia/core';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,17 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  /**
+   * Get an article with detailed info.
+   *
+   * @param section Target section
+   * @param id Target articles id
+   * @returns Detailed article info
+   */
+  @core.TypedRoute.Get(':id')
+  async test(@core.TypedParam('id', 'uuid') id: string) {
+    console.log('id', id);
   }
 }
